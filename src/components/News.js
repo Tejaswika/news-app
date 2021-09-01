a2e2f4f727025fe72986391bb9fbfa99f4995990
tejaswika <teju050301@gmail.com>
2021-09-02 01:06:17 +0530

    Page1 done

--- src/components/News.js
+++ src/components/News.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    
+    constructor(){
+        super();
+        this.state= {
+            articles: []
+        }
+    }
+    async componentDidMount(){
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=aa798d329d9e4cb28464cd4d462cdf94";
+        let data= await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles: parsedData.articles})
+    }
     render() {
+        
         return (
             <>
-              <NewsItem />  
+              <div className="row my-3 justify-content-center">
+                  {this.state.articles.map((element)=>{
+                      return(
+                        <div className="col-md-4 col-sm-6" key={element.url}>
+                            <NewsItem title={element.title.slice(0,51)} description={element.description.slice(0,65)} newsUrl={element.url} ImgUrl={element.urlToImage}/>
+                        </div>
+                      )
+                  })}
+                  
+              </div>
             </>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    render() {
-        return (
-            <>
-               this is News Item 
-            </>
-        )
-    }
+  render() {
+    let {title,description,newsUrl,ImgUrl}=this.props;
+    return (
+      <>
+        <div className="card" style={{margin:'10px'}}>
+          <img src={ImgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}....</h5>
+            <p className="card-text">{description}....
+            </p>
+            <a href={newsUrl} className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;
