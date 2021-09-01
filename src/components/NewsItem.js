f6c056049ad49bf21c904d8f42597fa4ff9efe5f
tejaswika <teju050301@gmail.com>
2021-09-01 17:10:36 +0530

    Basic layout

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        return (
+            <>
+               this is News Item 
+            </>
+        )
+    }
+}
+
+export default NewsItem
